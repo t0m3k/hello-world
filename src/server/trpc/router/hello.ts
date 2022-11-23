91e7e06993b96b4d50d01f3d913dbd85a68f6d4d
Tomasz Tracz <tomasz.tracz@2itesting.com>
2022-11-23 14:46:09 +0000

    Add hello api route

--- src/server/trpc/router/hello.ts
+++ src/server/trpc/router/hello.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 import { router, publicProcedure } from "../trpc";
 
-export const exampleRouter = router({
+export const helloRouter = router({
   hello: publicProcedure
     .input(z.object({ text: z.string().nullish() }).nullish())
     .query(({ input }) => {
